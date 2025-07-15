ac8956065df9d589b2409ca29f240a6e053d486a
CoderLi530 <Haopeoplezz@outlook.com>
2025-07-15 11:35:01 +0800

    cp问题

--- cp问题/2.blockqueue/Task.hpp
+++ cp问题/2.blockqueue/Task.hpp
@@ -1,24 +1,88 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+
+std::string opers = "+-*/%";
+
+enum
+{
+    DivZero = 1,
+    ModZero,
+    UnKnown
+};
 
 class Task
 {
-private:
-    int a;
-    int b;
-
 public:
-    Task(int x, int y):a(x), b(y)
+    Task(int x, int y, char oper):_data1(x), _data2(y), _oper(oper), _result(0), _exitcode(0) 
     {}
 
     void Run()
     {
-        std::cout << "run task: " << a << " + " << b << " = " << a + b << std::endl;
+        switch (_oper)
+        {
+            case '+':
+                _result = _data1 + _data2;
+                break;
+            case '-':
+                _result = _data1 - _data2;
+                break;
+            case '*':
+                _result = _data1 * _data2;
+                break;
+            case '/':
+                if (_data2 == 0) _exitcode = DivZero;
+                else _result = _data1 / _data2;
+                break;
+            case '%':
+                if (_data2 == 0) _exitcode = ModZero;
+                else _result = _data1 % _data2;
+                break;
+            default:
+                _exitcode = UnKnown;
+                break; 
+        }
+    }
+
+    //仿函数
+    void operator()()
+    {
+        Run();
+    }
+
+    //得到结果(计算表达式)
+    std::string GetResult()
+    {
+        std::string s = std::to_string(_data1);
+        s += _oper;
+        s += std::to_string(_data2);
+        s += " = ";
+        s += std::to_string(_result);
+        s += " [code: ";
+        s += std::to_string(_exitcode);
+        s += "] ";
+        return s;
+    }
+
+    std::string GetTask()
+    {
+        std::string s = std::to_string(_data1);
+        s += _oper;
+        s += std::to_string(_data2);
+        s += "= ? ";
+        return s;
     }
 
     ~Task()
     {
 
     }
+
+private:
+    int _data1; //数据1
+    int _data2; //数据2
+    char _oper; //运算符
+    int _result; //计算结果
+    int _exitcode; //返回码
 };
--- cp问题/2.blockqueue/main.cc
+++ cp问题/2.blockqueue/main.cc
@@ -1,18 +1,22 @@
 #include "BlockQueue.hpp"
 #include "Task.hpp"
 #include <unistd.h>
+#include <ctime>
 
 void* Consumer(void* args)
 {
     BlockQueue<Task>* bq = static_cast<BlockQueue<Task>*>(args);
     while (true)
     {
+        //sleep(1);
         //消费者消费
         //int data = bq->Pop();
         Task t = bq->Pop();
-        t.Run();
+        t();
         //std::cout << "消费了一个数据：" << data << std::endl;
+        std::cout << "处理任务" << t.GetTask() << "运算结果是：" << t.GetResult() << "thread id: " << pthread_self() << std::endl; 
         // sleep(2);
+       // sleep(1);
     }
 }
 
@@ -20,28 +24,54 @@ void* Producter(void* args)
 {
     BlockQueue<Task>* bq = static_cast<BlockQueue<Task>*>(args);
     //int data = 0;
-    int x = 20;
-    int y = 10;
+    //int x = 10;
+    //int y = 20;
+
     while (true)
     {
-        // sleep(2);
+        //sleep(1);
         //生产者生产
         //data++;
-        Task t(x, y);
+        usleep(100);
+        int x = rand() % 10 + 1;
+        int y = rand() % 10;
+        char op = opers[rand() % opers.size()];
+        Task t(x, y, op);
         bq->Push(t);
         //std::cout << "生产了一个数据：" << data << std::endl;
-        std::cout << "生产了一个任务：" << std::endl;
+        std::cout << "生产了一个任务：" << t.GetTask() << "thread id: " << pthread_self() << std::endl;
+        sleep(1);
     }
 }
 
 int main()
 {
+    srand(time(nullptr));
     BlockQueue<Task>* bq = new BlockQueue<Task>();
-    pthread_t c, p;
-    pthread_create(&c, nullptr, Consumer, bq);
-    pthread_create(&p, nullptr, Producter, bq);
-    pthread_join(c, nullptr);
-    pthread_join(p, nullptr);
+
+    pthread_t c[3], p[5];
+    for (int i = 0; i < 3; ++i)
+    {
+        pthread_create(c + i, nullptr, Consumer, bq);
+    }
+    for (int i = 0; i < 5; ++i)
+    {
+        pthread_create(p + i, nullptr, Producter, bq);
+    }
+    for (int i = 0; i < 3; ++i)
+    {
+        pthread_join(c[i], nullptr);
+    }
+    for (int i = 0; i < 5; ++i)
+    {
+        pthread_join(p[i], nullptr);
+    }
+
+    // pthread_t c, p;
+    // pthread_create(&c, nullptr, Consumer, bq);
+    // pthread_create(&p, nullptr, Producter, bq);
+    // pthread_join(c, nullptr);
+    // pthread_join(p, nullptr);
     delete bq;
     return 0;
 }
